b5eeabf332370f49104f1683cd73ce4fcecdeac6
github username <github email address>
2020-01-10 15:49:30 -0500

    Done.

--- Rails-Seven-Seas/app/controllers/application_controller.rb
+++ Rails-Seven-Seas/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index 
+    render :welcome
+  end
+
+  skip_before_action :verify_authenticity_token
+
 end
